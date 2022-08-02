a6795538b645d58fdcf15ac7e1c5c9f99cb5c4ef
William Gadney <gadnex@gmail.com>
2022-08-02 11:11:07 +0200

    Java 17 and WebSecurityConfigurerAdapter updatae

--- resource-server/src/main/java/net/binarypaper/demo/resourceserver/SecurityConfig.java
+++ resource-server/src/main/java/net/binarypaper/demo/resourceserver/SecurityConfig.java
@@ -1,19 +1,23 @@
 package net.binarypaper.demo.resourceserver;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableGlobalMethodSecurity(jsr250Enabled = true, prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
+        return http.build();
     }
 
     private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
